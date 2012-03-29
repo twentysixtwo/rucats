555ed9d8497db095e6613a7b0ec2d16f24e09cea
Aditya Devarakonda <aditya08@cac.rutgers.edu>
2012-03-29 13:06:43 -0400

    working version of checklogin.php uploaded. Now it interfaces with the database correctly.

--- web/checklogin.php
+++ web/checklogin.php
@@ -1,10 +1,10 @@
 <?php
 ob_start();
-$host="127.0.0.1"; // Host name 
-$username="test"; // Mysql username 
-$password="abc"; // Mysql password 
-$db_name="test"; // Database name 
-$tbl_name="members"; // Table name
+$host="hostname"; // Host name 
+$username="username"; // Mysql username 
+$password="password"; // Mysql password 
+$db_name="database"; // Database name 
+$tbl_name="table"; // Table name
 
 // Connect to server and select databse.
 mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
@@ -13,20 +13,17 @@ mysql_select_db("$db_name")or die("cannot select DB");
 // Define $myusername and $mypassword 
 $myusername=$_POST['myusername']; 
 $mypassword=$_POST['mypassword'];
-
 // To protect MySQL injection (more detail about MySQL injection)
 $myusername = stripslashes($myusername);
 $mypassword = stripslashes($mypassword);
 $myusername = mysql_real_escape_string($myusername);
 $mypassword = mysql_real_escape_string($mypassword);
 
-$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
+$sql="SELECT * FROM " . $tbl_name . " WHERE username='" . $myusername . "' and password='". $mypassword. "'";
 $result=mysql_query($sql);
-
 // Mysql_num_row is counting table row
 $count=mysql_num_rows($result);
 // If result matched $myusername and $mypassword, table row must be 1 row
-
 if($count==1){
 // Register $myusername, $mypassword and redirect to file "login_success.php"
 session_register("myusername");
@@ -36,6 +33,5 @@ header("location:login_success.php");
 else {
 echo "Wrong Username or Password";
 }
-
 ob_end_flush();
 ?>
