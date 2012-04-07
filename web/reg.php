2a2e5abe35c815764c55b405019a6e0b8185f2c2
Jon Huang <jonathanxhuang@gmail.com>
2012-04-07 19:09:16 -0400

    registration adds to the members table but doesnt verify username/password validity properly

--- web/reg.php
+++ web/reg.php
@@ -2,50 +2,44 @@
 	ob_start();
 	session_start();
 		
-	if ($_POST["act"]=="reg") { /// do after login form is submitted
+	if ($_POST["act"]=="reg") {
+		 	if(($_POST['password'])==($_POST['password2'])) {
+			$host="localhost"; // Host name 
+			$username="user"; // Mysql username 
+			$password=""; // Mysql password 
+			$db_name="test"; // Database name 
+			$tbl_name="members"; // Table name
+
+			// Connect to server and select databse.
+			mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+			mysql_select_db("$db_name")or die("cannot select DB");
+
+			// Define $myusername and $mypassword 
+			$myusername=$_POST['username']; 
+			$mypassword=$_POST['password'];
+			// To protect MySQL injection
+			$myusername = stripslashes($myusername);
+			$mypassword = stripslashes($mypassword);
+			$myusername = mysql_real_escape_string($myusername);
+			$mypassword = mysql_real_escape_string($mypassword);
+
+			$sql="SELECT * FROM " . $tbl_name . " WHERE username='" . $myusername;
+			$result=mysql_query($sql);
+			// Mysql_num_row is counting table row
+			$count=mysql_num_rows($result);
+			if($count==1){ // username is used already
+				sleep(5);
+				header("location: register.php"); 
+			} else { //username and password valid put in db
+				mysql_query("INSERT INTO members (username, password) values ('" . $myusername . "','" . $mypassword . "')");
+				header("location: register.php");
+			}
+		} else { //passwords not equal
+			sleep(5);
+			header("location: register.php");
+		}			 
+
+}
 	
-	$host="localhost"; // Host name 
-	$username="root"; // Mysql username 
-	$password="a"; // Mysql password 
-	$db_name="test"; // Database name 
-	$tbl_name="members"; // Table name
-
-	// Connect to server and select databse.
-	mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-	mysql_select_db("$db_name")or die("cannot select DB");
-
-	// Define $myusername and $mypassword 
-	$myusername=$_POST['username']; 
-	$mypassword=$_POST['password'];
-	// To protect MySQL injection
-	$myusername = stripslashes($myusername);
-	$mypassword = stripslashes($mypassword);
-	$myusername = mysql_real_escape_string($myusername);
-	$mypassword = mysql_real_escape_string($mypassword);
-
-	$sql="SELECT * FROM " . $tbl_name . " WHERE username='" . $myusername . "' and password='". $mypassword. "'";
-	$result=mysql_query($sql);
-	// Mysql_num_row is counting table row
-	$count=mysql_num_rows($result);
-	// If result matched $myusername and $mypassword, table row must be 1 row
-	if($count==1){
-		$_SESSION["logged"]=$_POST["username"];
-		header("location: index.php"); 
-	} else { 
-		echo 'Incorrect username/password. Please, try again.'; 
-	}; 
-
-};
-	header("location:index.php");
-	//if ((array_key_exists($_SESSION["logged"],$USERS)) && (!empty($_SESSION["logged"]))) { 
-	//	header("location: index.php");
-	//} else { // if not logged show login form (eventually redirect to prettier login page) 
-	
-		//echo '<form action="login.php" method="post"><input type="hidden" name="act" value="log"> '; 
-     	//echo 'Username: <input type="text" name="username" /><br />'; 
-     	//echo 'Password: <input type="password" name="password" /><br />'; 
-     	//echo '<input type="submit" value="Login" />'; 
-     	//echo '</form>'; 
-	//}; 
 ob_end_flush();
 ?>
